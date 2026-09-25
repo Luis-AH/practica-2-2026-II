a277ef0a6c9d829a97d7ae8eac56a40ebc0140ad
Luis-AH <luis_asin@usmp.pe>
2026-09-25 01:19:45 -0500

    Fase 6: Notificaciones en tiempo real con PieSocket y Pusher

--- obj/Debug/net10.0/PlataformaCreditos.AssemblyInfo.cs
+++ obj/Debug/net10.0/PlataformaCreditos.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlataformaCreditos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0df6f661c525b3800fdb1570816dbc4510f3504")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ba3df506a2b6976f5636e962761fa50203d2da8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlataformaCreditos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlataformaCreditos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
